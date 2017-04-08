d32d773dd9216db72bef86d94168d1fb53399c6f
Rik van der Heijden <mail@rikvanderheijden.com>
2017-04-08 15:48:20 +0200

    Change the way golang packages are built

--- lib/buildcfg/cfg_lang_go.go
+++ lib/buildcfg/cfg_lang_go.go
@@ -5,21 +5,28 @@ import (
 	"strings"
 )
 
+func goImportPath(remote string) string {
+	if strings.Contains(remote, ":") && strings.Contains(remote, "@") {
+		rem := remote[strings.Index(remote, "@")+1:]
+		return strings.Replace(strings.Replace(rem, ".git", "", 1), ":", "/", 1)
+	}
+	u, err := url.Parse(remote)
+	if err != nil {
+		return remote
+	}
+	return u.Hostname() + strings.Replace(u.Path, ".git", "", 1)
+}
+
 func loadGoConfig(remote string, c *Config) {
-	u, _ := url.Parse(remote)
-	importPath := u.Hostname() + strings.Replace(u.Path, ".git", "", 1)
+	importPath := goImportPath(remote)
 
 	// Most part of this just moves everything from builddir to the correct go-import-path
 	setup := []string{
-		"export GOPATH=/build",
+		"export GOPATH=/build/.gopath",
 		"export PATH=$PATH:/build/bin",
-		"mkdir /tmp/dat",
-		"rsync -az . /tmp/dat",
-		"rm -rf *",
-		"mkdir -p src/" + importPath,
-		"rsync -az /tmp/dat/ src/" + importPath,
-		"rm -rf /tmp/dat",
-		"cd src/" + importPath,
+		"mkdir -p /build/.gopath/src/" + importPath,
+		"mount -o bind /build /build/.gopath/src/" + importPath,
+		"cd /build/.gopath/src/" + importPath,
 	}
 	c.Setup.V = append(setup, c.Setup.V...)
 	c.Addons.Apt.Packages = append(c.Addons.Apt.Packages, "rsync")
