c951d643f9dca133e85a26fb0c82173bac59b488
Rik van der Heijden <mail@rikvanderheijden.com>
2017-04-10 06:53:54 +0200

    Workaround for now

--- lib/builder/artifacts.go
+++ lib/builder/artifacts.go
@@ -43,33 +43,34 @@ func copyFile(src, dst string) (err error) {
 
 func handleArtifacts(f *os.File, job *models.Job, cfg *buildcfg.Config) {
 	artifactDir := filepath.Join(buildDir, "artifacts", strconv.Itoa(int(job.ID)))
-	if len(cfg.Addons.Artifacts.Paths) > 0 {
-		os.Mkdir(artifactDir, 0755)
+	if cfg != nil {
+		if len(cfg.Addons.Artifacts.Paths) > 0 {
+			os.Mkdir(artifactDir, 0755)
+		}
+
+		for _, artifactsPath := range cfg.Addons.Artifacts.Paths {
+			paths, err := filepath.Glob(filepath.Join(job.BuildDir, artifactsPath))
+			if err != nil {
+				fmt.Fprintf(f, "Error searching for artifacts: %v\n", err)
+				job.SetStatus(models.StatusFailed, "Missing artifacts")
+				return
+			}
+
+			fmt.Println(filepath.Join(job.BuildDir, artifactsPath))
+			if len(paths) == 0 {
+				fmt.Fprintf(f, "Expected artifacts at %s, found none\n", artifactsPath)
+				job.SetStatus(models.StatusFailed, "Missing artifacts at "+artifactsPath)
+				return
+			}
+
+			for _, file := range paths {
+				fi := strings.Replace(file, job.BuildDir+"/", "", 1)
+				artifactFile := filepath.Join(artifactDir, fi)
+				copyFile(file, artifactFile)
+				models.Handle().Create(&models.Artifact{FilePath: fi, JobID: job.ID})
+				fmt.Fprintf(f, "Archiving file: %s\n", fi)
+			}
+		}
 	}
-
-	for _, artifactsPath := range cfg.Addons.Artifacts.Paths {
-		paths, err := filepath.Glob(filepath.Join(job.BuildDir, artifactsPath))
-		if err != nil {
-			fmt.Fprintf(f, "Error searching for artifacts: %v\n", err)
-			job.SetStatus(models.StatusFailed, "Missing artifacts")
-			return
-		}
-
-		fmt.Println(filepath.Join(job.BuildDir, artifactsPath))
-		if len(paths) == 0 {
-			fmt.Fprintf(f, "Expected artifacts at %s, found none\n", artifactsPath)
-			job.SetStatus(models.StatusFailed, "Missing artifacts at "+artifactsPath)
-			return
-		}
-
-		for _, file := range paths {
-			fi := strings.Replace(file, job.BuildDir+"/", "", 1)
-			artifactFile := filepath.Join(artifactDir, fi)
-			copyFile(file, artifactFile)
-			models.Handle().Create(&models.Artifact{FilePath: fi, JobID: job.ID})
-			fmt.Fprintf(f, "Archiving file: %s\n", fi)
-		}
-	}
-
 	job.SetStatus(models.StatusPassed)
 }
