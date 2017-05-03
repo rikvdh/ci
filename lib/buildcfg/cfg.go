9946da59527e4233c02db208040681712f4afa06
Rik van der Heijden <mail@rikvanderheijden.com>
2017-05-03 06:55:59 +0200

    Fix config script

--- lib/buildcfg/cfg.go
+++ lib/buildcfg/cfg.go
@@ -61,7 +61,7 @@ func isValidCommand(cmd string) bool {
 }
 
 func writeFunc(f io.Writer, name string, script []string) {
-	io.WriteString(f, name+"() (\nset+ve\n")
+	io.WriteString(f, name+"() (\nset +ve\n")
 	for _, s := range script {
 		if isValidCommand(s) {
 			io.WriteString(f, s+"\n")
