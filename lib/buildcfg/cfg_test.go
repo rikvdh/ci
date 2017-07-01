c82b91a7a6854aa3008aa760982c5f1922ae9c79
Rik van der Heijden <mail@rikvanderheijden.com>
2017-07-01 15:50:07 +0200

    Improve test

--- lib/buildcfg/cfg_test.go
+++ lib/buildcfg/cfg_test.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConfigRead(t *testing.T) {
@@ -13,32 +15,58 @@ script:
 - foo`
 
 	err := ioutil.WriteFile(".ci.yml", []byte(s), 0644)
-	if err != nil {
-		panic(err)
-	}
+	assert.Nil(t, err)
+	defer os.Remove(".ci.yml")
+
+	cfg := Read(".", "boembats")
+
+	assert.Equal(t, cfg.DockerImage, "test", "Expected docker-image to be test")
+	assert.Equal(t, len(cfg.Script.V), 2, "Expected exactly 2 script lines")
+	assert.Equal(t, cfg.Script.V[0], "boom", "expected 1st script-item to be boom")
+	assert.Equal(t, cfg.Script.V[1], "foo", "expected 2nd script-item to be foo")
+}
+
+func TestDefault(t *testing.T) {
+	cfg := Read("/non/existing/file", "bots")
+	assert.Equal(t, cfg.DockerImage, "debian", "Expected docker-image to be debian")
+	assert.Equal(t, len(cfg.Script.V), 0, "Expected empty script")
+}
+
+func TestWildArtifactsAndApt(t *testing.T) {
+	s := `language: go
+
+go:
+- go1.8
+- tip
+
+addons:
+  artifacts:
+    paths:
+    - '*.deb'
+  apt:
+    packages:
+    - libgsf-1-dev
+    - libgsf-1-common
+
+script:
+- ./build/libvips.sh
+- go get -t -v ./...
+- go test -race -v $(go list ./... | grep -v /vendor/)
+- go get -v -u github.com/alecthomas/gometalinter
+- gometalinter --install --update
+- gometalinter --deadline=600s --sort=path --vendor ./... || true
+- go get -v -u github.com/xor-gate/debpkg/cmd/debpkg
+- debpkg
+`
+	err := ioutil.WriteFile(".ci.yml", []byte(s), 0644)
+	assert.Nil(t, err)
 
 	defer os.Remove(".ci.yml")
 
 	cfg := Read(".", "boembats")
 
-	if cfg.DockerImage != "test" {
-		t.Error("Expected docker-image to be test")
-	}
-	if len(cfg.Script.V) != 2 {
-		t.Error("Expected exactly 2 script lines")
-	}
-	if cfg.Script.V[0] != "boom" || cfg.Script.V[1] != "foo" {
-		t.Error("expected boom foo")
-	}
-}
-
-func TestDefault(t *testing.T) {
-	cfg := Read("/non/existing/file", "bots")
-
-	if cfg.DockerImage != "debian" {
-		t.Error("Expected docker-image to be debian")
-	}
-	if len(cfg.Script.V) != 0 {
-		t.Error("Expected no script lines")
-	}
+	assert.Equal(t, cfg.DockerImage, "golang", "expected golang docker image")
+	assert.Equal(t, "go", cfg.Language)
+	assert.Equal(t, "boembats", cfg.GoImportPath)
+	assert.Equal(t, "*.deb", cfg.Addons.Artifacts.Paths[0])
 }
