5d64dd8a26208a883aaab74986d68871589f85d0
Rik van der Heijden <mail@rikvanderheijden.com>
2017-07-01 17:55:14 +0200

    Change ci

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/rikvdh/ci/lib/builder"
 	"github.com/rikvdh/ci/lib/config"
 	"github.com/rikvdh/ci/lib/indexer"
@@ -8,7 +9,11 @@ import (
 	"github.com/rikvdh/ci/web"
 )
 
+var buildDate string
+var buildVersion = "dev"
+
 func main() {
+	logrus.Infof("rikvdh/ci started version %s (built at %s)", buildVersion, buildDate)
 	config.Load("ci.ini")
 	models.Init()
 
